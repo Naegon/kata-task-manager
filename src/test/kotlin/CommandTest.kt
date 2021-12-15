5f8c5f6d2ec21b78a5b38a37513fcf376de4f14b
Naegon <hamelgui@gmail.com>
2021-12-15 15:46:20 +0100

    Renamed KataParsertest to CommandTest

--- src/test/kotlin/CommandTest.kt
+++ src/test/kotlin/CommandTest.kt
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class KataParserTest {
+internal class CommandTest {
     @Test
     fun parse_WithAdd() {
         // Given
